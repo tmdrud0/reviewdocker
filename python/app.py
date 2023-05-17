f8e9c70565a427dcd1ae0d8e487899450eab977f
tmdrud0 <tmdrud049@gmail.com>
2023-05-17 09:57:52 +0900

    ff

---
+++ python/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/test')
+def hello():
+    return 'Hello Flask World'
+
+@app.route('/product')
+def product():
+    response = {
+        'name': '테스트 이름',
+        'price': '테스트 가격',
+        'category': '테스트 카테고리'
+    }
+
+    return jsonify(response), 200
+
+@app.route('/api')
+def test():
+    return 'hello test'
+
+    
+if __name__ == '__main__':
+    app.run()
